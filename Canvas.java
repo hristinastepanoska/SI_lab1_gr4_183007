ead2b71dc417857a8b0be14b88ead53725aa6e2f
hristinastepanoska <63523153+hristinastepanoska@users.noreply.github.com>
2020-04-12 23:25:13 +0200

    new

---
+++ Canvas.java
@@ -0,0 +1,15 @@
+public class Canvas {
+	List<Points>
+}
+class SLL<E> {
+	private SLLNode<E> first;
+
+	public SLL() {
+		// Construct an empty SLL
+		this.first = null;
+	}
+
+	public void deleteList() {
+		first = null;
+	}
+}
